c8e04b81887ffd6d5e63a7b089bfa8476c4f8d49
JunWonOh <junoh9847@gmail.com>
2021-08-05 21:03:32 -0400

    Edit directories, added homepage and templates

--- app.js
+++ app.js
@@ -6,10 +6,13 @@ const express = require('express');
 const ejs = require('ejs');
 
 const app = express();
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
 
 
-
-
+app.get("/", function(req, res){
+    res.render("home")
+})
 
 app.listen(port, function(){
     console.log("Server initialized on port " + port);
